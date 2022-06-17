857a60de8f2eafbdb35dbf6e231bd3afcf4c7f74
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-06-18 01:20:25 +0300

    extracting header/summary/byline

--- test_html.py
+++ test_html.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 import requests
 from bs4 import BeautifulSoup
@@ -8,7 +9,7 @@ from urllib.parse import urlparse
 class TestIndex(unittest.TestCase):
 
     def test_index_html(self):
-        url = "https://www.theverge.com/tech/"
+        url = "https://www.theverge.com/21546591/best-amazon-echo-deals"
 
         HEADERS = ({'User-Agent':
                         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
@@ -17,16 +18,14 @@ class TestIndex(unittest.TestCase):
 
         html_text = requests.get(url, headers=HEADERS).text
         html_text = html_text.replace(">", ">\n")
-        with open(f"tech.html", "w") as f:
+        with open(f"article.html", "w") as f:
             f.write(html_text)
 
         # text = BeautifulSoup(html_text).get_text()
         # with open("tech.txt", "w") as f:
         #     f.write(text)
 
-
-
-    def test_main_menu_header(self):
+    def test_fain_main_menu_header_links(self):
         with open("index.html", "r") as f:
             content = f.read()
             soup = BeautifulSoup(content)
@@ -50,8 +49,7 @@ class TestIndex(unittest.TestCase):
                     href = li.select("a")[0].get("href")
                     folder2href[active_folder].append(href)
 
-
-    def test_folder_link(self):
+    def test_find_folder_link(self):
         with open("tech.html", "r") as f:
             content = f.read()
             soup = BeautifulSoup(content)
@@ -62,3 +60,22 @@ class TestIndex(unittest.TestCase):
             for a in r:
                 links.append(a.get("href"))
             pass
+
+    def test_parse_article(self):
+        with open("article.html", "r") as f:
+            content = f.read()
+            soup = BeautifulSoup(content)
+            header_wrap = soup.select("article.l-main-content "
+                                      "div.c-entry-hero__header-wrap")
+
+            if len(header_wrap) == 0:
+                raise RuntimeError("no item found")
+
+            header_text = header_wrap[0].select("h1")[0].text
+            summary_text = soup.select("article.l-main-content p.c-entry-summary")[0].text
+            authors_and_time = soup.select("article.l-main-content div.c-byline")[0].text
+            authors_and_time = authors_and_time.replace("\n", "")
+            authors_and_time = re.sub("\s{2,}", " ", authors_and_time).strip()
+            pass
+
+
