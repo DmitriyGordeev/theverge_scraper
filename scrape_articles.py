f382e8ff524336544b2042087611b3ee4da60be8
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-07-23 16:04:56 +0300

    scrape_articles.py with global path

--- scrape_articles.py
+++ scrape_articles.py
@@ -2,11 +2,12 @@ from html_parser import *
 import json
 from postgre_db_interface import *
 from scraper import *
+from settings import *
 
 
 if __name__ == "__main__":
 
-    f = open("topic2articles.json", "r")
+    f = open(Settings.global_path + "topic2articles.json", "r")
     topics2articles = json.loads(f.read())
     f.close()
 
@@ -14,7 +15,7 @@ if __name__ == "__main__":
 
     Path(scraper.root_output_dir).mkdir(parents=True, exist_ok=True)
     Path(scraper.root_output_dir + "/articles").mkdir(parents=True, exist_ok=True)
-    Path("errors/").mkdir(parents=True, exist_ok=True)
+    Path(Settings.global_path + "errors/").mkdir(parents=True, exist_ok=True)
 
     scraper.loop_through_articles(topics2articles)
 
