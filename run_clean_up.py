c6044ccf176970a7686fb3de73f781fa6a135c29
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-07-27 22:53:23 +0300

    cleanup script

---
+++ run_clean_up.py
@@ -0,0 +1,11 @@
+from settings import Settings
+import os
+
+
+def main():
+    os.system(f"rm {Settings.global_path}/data/articles/*.json")
+    os.system(f"rm {Settings.global_path}/topic2articles.json")
+
+
+if __name__ == "__main__":
+    main()
